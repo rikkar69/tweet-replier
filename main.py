edcd8572b707cac6706de69e12b4b1b1e3ce531f
Rikkar <95973743+rikkar69@users.noreply.github.com>
2023-04-16 17:21:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,18 +9,21 @@ openai_api_key = st.secrets["API_KEY"]
 #prompt template to pass to langchain
 template = """
     Below is an example of a tweet. Your goal is to:
-    -write a tweet to send as a reply in the tone that is selected
+    -construct a thoughtful and engaging reply to the below tweet. The tone of the tweet should be {tone}
     -if the tweet is a question, craft a reply to the question being asked
     -if the tweet is a statement, craft a reply that carries on the conversation
     -make it relevant to the tweet that is being replied to
-    -keep it concise and not too long
+    -Address the main points of the tweet.
+    -Use concise and clear language
     -do not include hashtags or the charactor #
 
-    Here are some examples of different tones:
-    -Funny: If only I could be as productive as I am when I'm avoiding work.
-    -Formal: I recognize that I could improve my productivity, and I am actively seeking ways to do so.
-    -Funny: I'd like to thank my morning coffee for giving me the motivation to pretend like I know what I'm doing.
-    -Formal: I am grateful for the caffeine boost provided by my morning coffee, which allows me to approach my work with confidence.
+    Here are some examples of tweets and replies in different tones:
+    -Tweet #1: Happy monday! Mondays are the worst.
+    -Funny reply #1: If only I could be as productive as I am when I'm avoiding work.
+    -Formal reply #1: I recognize that I could improve my productivity, and I am actively seeking ways to do so.
+    -Tweet #2: What are you grateful for today?
+    -Funny reply #2: I'd like to thank my morning coffee for giving me the motivation to pretend like I know what I'm doing.
+    -Formal reply #2: I am grateful for the caffeine boost provided by my morning coffee, which allows me to approach my work with confidence.
 
     Below is the tweet and tone:
     TONE: {tone}
